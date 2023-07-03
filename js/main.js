69b87c814377175986f1dfcdfd9ce53b285f8be1
Matteojk95 <matteopalmer2095@gmail.com>
2023-07-03 21:00:32 +0200

    finish, bad work

--- js/main.js
+++ js/main.js
@@ -11,24 +11,27 @@ function submitForm(event) {
 
 // chiedo all'utente il numero di chilometri che vuole percorrere e l'età del passeggero (con i prompt() in questo caso)
 
+
 let numerochilometri= prompt (" inserisci il numero di chilometri che devi fare");
 let etapasseggero= prompt ("inserisci la tua età");
 
 numerochilometri=parseInt (numerochilometri);
+etapasseggero= parseInt (latuaeta);
 
 /*Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
 */
 
-
 let inputchilometri= document.getElementById("inputchilometri").value;
-let etapassenger= document.getElementById("inputetapassenger").value;
+let inputetapassenger= document.getElementById("inputetapassenger").value;
+
+
 
 let pricevoyage= (inputchilometri *0,21);
 
-if (etapassenger < 18) {
+if (inputetapassenger < 18) {
     pricevoyage * 20 / 100
-} else if (etapassenger > 65) {
+} else if (inputetapassenger > 65) {
     pricevoyage * 40 / 100
 } else {
     pricevoyage
