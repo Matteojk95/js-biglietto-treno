813507873eceb64c9f792701e65455e23024f274
Matteojk95 <matteopalmer2095@gmail.com>
2023-07-03 18:29:59 +0200

    new

--- js/main.js
+++ js/main.js
@@ -9,9 +9,9 @@ numerochilometri=parseInt (numerochilometri);
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
 */
 
-let numerochilometriinput= document.getElementById("inputchilometri").value;
+let inputchilometri= document.getElementById("inputchilometri").value;
 
-let pricevoyage= (numerochilometriinput *0,21);
+let pricevoyage= (inputchilometri *0,21);
 
 if (etapasseggero <= 18) {
     pricevoyage* 20 / 100
